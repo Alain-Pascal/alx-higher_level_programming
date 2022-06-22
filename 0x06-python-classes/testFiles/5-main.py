1691646aad805d96e0587ccb2e43b55b6b7091d2
AlainPascal <ialainpascal@gmail.com>
2022-06-23 02:45:34 +0300

    Write a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/testFiles/5-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+sys.path.append('../0x06-python-classes')
+Square = __import__('5-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
