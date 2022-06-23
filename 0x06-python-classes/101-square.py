070404997058fdc79d3320ac3382e3196ebe7680
AlainPascal <ialainpascal@gmail.com>
2022-06-23 03:42:20 +0300

    Write a class Square that defines a square by: (based on 6-square.py)

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+"""Square module
+This module contains a class that defines a square
+and initializes it with a size
+"""
+
+
+class Square:
+    """Defines a square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize Square objects
+
+        Args:
+            size (int): size of the square
+            position (int, int): coordinates of the square
+        """
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Public instance method
+
+        Returns:
+            the current square area
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Getter method that retrieves the size of square
+
+        Returns:
+            size of the square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter method that sets size of square to a value
+
+        Args:
+            value (int): value to set as size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter method to retrieve position of square
+
+        Returns:
+            position of square
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """Setter method that sets the position of the square
+        """
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """Prints in stdout the square with the character #
+        """
+        if (self.__size == 0):
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for k in range(0, self.__position[0])]
+            [print("#", end="") for j in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        """Defines the print representation of square
+        """
+        if self.__size != 0:
+            [print("", end="") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for k in range(0, self.__position[0])]
+            [print("#", end="") for j in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
---
+++ 0x06-python-classes/testFiles/101-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+sys.path.append('../0x06-python-classes')
+Square = __import__('101-square').Square
+
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
