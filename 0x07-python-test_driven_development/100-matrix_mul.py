eef6a1fe81016c0b0b6a5d6d4b7a02b11982d61c
Alain Pascal <ialainpascal@gmail.com>
2025-04-16 01:23:01 +0300

    Validate pycodestyle

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -29,24 +29,24 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    
+
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    
+
     if not m_a or not m_a[0]:
         raise ValueError("m_a can't be empty")
     if not m_b or not m_b[0]:
         raise ValueError("m_b can't be empty")
-    
+
     for row in m_a:
         if not all(isinstance(ele, (int, float)) for ele in row):
             raise TypeError("m_a should contain only integers or floats")
     for row in m_b:
         if not all(isinstance(ele, (int, float)) for ele in row):
             raise TypeError("m_b should contain only integers or floats")
-    
+
     len_row_a = len(m_a[0])
 
     if not all(len(row) == len_row_a for row in m_a):
@@ -56,10 +56,10 @@ def matrix_mul(m_a, m_b):
 
     if not all(len(row) == len_row_b for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
-    
+
     if len_row_a != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    
+
     # Preform matrix multiplication
     result = [[0 for _ in range(len_row_b)] for _ in range(len(m_a))]
 
@@ -67,5 +67,5 @@ def matrix_mul(m_a, m_b):
         for j in range(len_row_b):
             for k in range(len(m_b)):
                 result[i][j] += m_a[i][k] * m_b[k][j]
-    
+
     return result
