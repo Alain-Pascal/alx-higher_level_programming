26dda7e3e78cd15eb9f58ad8b013f999bc8ba1e8
Alain Pascal <ialainpascal@gmail.com>
2025-04-19 02:29:01 +0300

    Write a function that prints a square with the char #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This  module defines a function that 
+prints a square with the char #.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the char #
+
+    Args:
+        size (int): lengrg of the square
+
+    Raises:
+        TypeError: If size is not an int
+        ValueError: If size is less than 0
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
