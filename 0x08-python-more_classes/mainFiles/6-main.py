7b1e4216334c91d699c7ece62d5ac1f30c33bffd
Alain Pascal <ialainpascal@gmail.com>
2025-04-23 23:57:45 +0300

    Write a class Rectangle that defines a rectangle based on 5-rectangle.py

--- 0x08-python-more_classes/mainFiles/6-main.py
+++ 0x08-python-more_classes/mainFiles/6-main.py
@@ -2,4 +2,17 @@
 import sys
 
 sys.path.append("../0x08-python-more_classes")
+Rectangle = __import__('6-rectangle').Rectangle
 
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+
+del my_rectangle_1
+
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+
+del my_rectangle_2
+
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
