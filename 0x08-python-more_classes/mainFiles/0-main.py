19dbb5f7a4aa6c616620c1b1651f62e01c732674
Alain Pascal <ialainpascal@gmail.com>
2025-04-23 12:24:47 +0300

    Write an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/mainFiles/0-main.py
+++ 0x08-python-more_classes/mainFiles/0-main.py
@@ -2,3 +2,10 @@
 import sys
 
 sys.path.append("../0x08-python-more_classes")
+
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
