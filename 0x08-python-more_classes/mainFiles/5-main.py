3f360583470faa26518bbe6baf05c867eee98856
Alain Pascal <ialainpascal@gmail.com>
2025-04-23 23:31:25 +0300

    Write a class Rectangle that defines a rectangle based on 4-rectangle.py

--- 0x08-python-more_classes/mainFiles/5-main.py
+++ 0x08-python-more_classes/mainFiles/5-main.py
@@ -2,4 +2,15 @@
 import sys
 
 sys.path.append("../0x08-python-more_classes")
+Rectangle = __import__('3-rectangle').Rectangle
 
+my_rectangle = Rectangle(2, 4)
+
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+del my_rectangle
+
+try:
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
