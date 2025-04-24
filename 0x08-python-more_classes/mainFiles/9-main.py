6fc87846dab11ab4dca5f9cabe8216fb4a766e8c
Alain Pascal <ialainpascal@gmail.com>
2025-04-24 11:51:11 +0300

    Write a class Rectangle that defines a rectangle based on 8-rectangle.py

--- 0x08-python-more_classes/mainFiles/9-main.py
+++ 0x08-python-more_classes/mainFiles/9-main.py
@@ -2,4 +2,12 @@
 import sys
 
 sys.path.append("../0x08-python-more_classes")
+Rectangle = __import__('9-rectangle').Rectangle
 
+my_square = Rectangle.square(5)
+
+print("Area: {} - Perimeter: {}".format(
+    my_square.area(), my_square.perimeter())
+)
+
+print(my_square)
