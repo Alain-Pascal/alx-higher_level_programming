7bbcc106f93faf2c890e101f9058a260f5a523ff
Alain Pascal <ialainpascal@gmail.com>
2025-05-03 16:59:16 +0300

    Low memory cost

---
+++ 0x09-python-everything_is_object/mainFiles/101-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+sys.path.append("../0x09-python-everything_is_object")
+
+LockedClass = __import__('101-locked_class').LockedClass
+
+lc = LockedClass()
+lc.first_name = "John"
+
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
