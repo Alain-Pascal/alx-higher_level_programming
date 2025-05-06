7ce99e9072895c9443c66bf985b45161e833066f
Alain Pascal <ialainpascal@gmail.com>
2025-05-06 08:53:35 +0300

    Write function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+This function returns the list of available attributes and
+methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Returns a list of objects containing the names of attributes
+    and methods of the given object.
+
+    Args:
+        obj: The object whose attributes and methods are to be listed.
+
+    Returns:
+        list: A list of strings representing the names of attributes
+              and methods of the object.
+    """
+    return dir(obj)
--- 0x0A-python-inheritance/mainFiles/0-main.py
+++ 0x0A-python-inheritance/mainFiles/0-main.py
@@ -3,3 +3,22 @@ import sys
 
 sys.path.append("../0x0A-python-inheritance")
 
+lookup = __import__("0-lookup").lookup
+
+
+class MyClass1(object):
+    """MyClass1"""
+    pass
+
+
+class MyClass2(object):
+    my_attr1 = 3
+
+    def my_meth(self):
+        """My method"""
+        pass
+
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
