5061c350bfacc12d53588b7f66b72beb800c22ff
Alain Pascal <ialainpascal@gmail.com>
2025-05-06 10:53:44 +0300

    Write an empty class

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry."""
+    pass
--- 0x0A-python-inheritance/mainFiles/5-main.py
+++ 0x0A-python-inheritance/mainFiles/5-main.py
@@ -2,3 +2,10 @@
 import sys
 
 sys.path.append("../0x0A-python-inheritance")
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
