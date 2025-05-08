efed2f6a75bc26d9bf8d29866d3c7ae9552d8e27
Alain Pascal <ialainpascal@gmail.com>
2025-05-08 21:23:15 +0300

    update json file path

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "./files/add_item.json"
+filename = "add_item.json"
 my_list = []
 
 if path.exists(filename):
