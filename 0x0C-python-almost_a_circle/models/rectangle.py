845aaec76d1acbb532ae4a8f266e6379e74a8eee
Alain Pascal <ialainpascal@gmail.com>
2025-05-09 11:27:21 +0300

    Write the class Rectangle that inherits from Base

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/user/bin/python3
+"""
+Defines a Rectangle class that inherits from Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class that inherits from Base
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a new Rectangle instance
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+            x (int): The x coordinate of the rectangle
+            y (int): The y coordinate of the rectangle
+            id (int): The id of the rectangle. If None, the id is set to
+            the next available id.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    # getter and setter for width
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    # getter and setter for height
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    # getter and setter for x
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    # getter and setter for y
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
